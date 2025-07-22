08afb409ab172a0c0a877b50deb1ad9d30cd29c0
jonxlegasa <jon.legasa@gmail.com>
2025-07-22 01:40:59 -0400

    This Symbolic.jl experiment helped me learn a lot... however it did not fit the vision

--- symbolicsfailure.jl
+++ symbolicsfailure.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 using Symbolics
 using Random
 
-@variables n l
+@variables n l 
 @variables a[0:100]  # Array of symbolic coefficients
 
 function get_user_inputs()
@@ -23,12 +23,13 @@ function get_user_inputs()
   return ode_order, poly_degree, dataset_size
 end
 
-function create_symbolic_coefficients(max_index)
-  coeffs = Dict()
-  for i in 0:max_index
-    coeffs[i] = only(@variables $(Symbol("a_$i")))
-  end
-  return coeffs
+function create_coefficient_variables(polynomial_degree)
+  indices = collect(0:polynomial_degree)  # Start from 0, go up to n+degree
+  
+  coeff_vars = Symbolics.variables(:a_n, indices)
+  println(coeff_vars)
+  
+  return coeff_vars
 end
 
 function factorial_product(n, k, i)
@@ -42,42 +43,41 @@ function factorial_product(n, k, i)
   return product
 end
 
-function matrix_element(n, k, i)
-  # From the pattern: row k, column i
-  # Element is: ∏(n + j - i) for j = 1 to k, times a_{n + k - i}
-  # Create the coefficient symbol a_{n + k - i}
+
+function matrix_element(n, k, i, coeff_vars)
   offset = k - i
-  if offset == 0
-    coeff_symbol = only(@variables a_n)
-  elseif offset > 0
-    coeff_symbol = only(@variables $(Symbol("a_n+$offset")))
-  else  # offset < 0
-    coeff_symbol = only(@variables $(Symbol("a_n$offset")))  # negative already included
-  end
+  coeff_symbol = coeff_vars[n + offset]
   return factorial_product(n, k, i) * coeff_symbol
+  
 end
 
 
-function construct_recurrence_matrix(n_symbolic, matrix_size)
-  # coeffs = create_symbolic_coefficients(matrix_size)
-  recurrence_matrix = Matrix{Any}(nothing, matrix_size, matrix_size)
 
+# Then clean up the final result's display
+function clean_recurrence_display(recurrence)
+  clean_str = replace(string(recurrence), r"Symbol\(([^)]+)\)" => s"\1")
+  return clean_str
+end
+
+function construct_recurrence_matrix(n_symbolic, matrix_size, polynomial_degree)
+  coeff_vars = create_coefficient_variables(polynomial_degree)
+  recurrence_matrix = Matrix{Any}(nothing, matrix_size, matrix_size)
+  
   for k in 0:(matrix_size-1)
     for i in 0:(matrix_size-1)
-      # recurrence_matrix[k+1, i+1] = matrix_element(n_symbolic, k, i, coeffs)
-      recurrence_matrix[k+1, i+1] = matrix_element(n_symbolic, k, i)
+        recurrence_matrix[k+1, i+1] = matrix_element(n_symbolic, k, i, coeff_vars)
     end
   end
-
+  
   return recurrence_matrix
 end
 
 
-function generate_recurrence_relation(α_matrix)
+function generate_recurrence_relation(α_matrix, polynomial_degree)
   matrix_size = size(α_matrix, 1)
   println("α_matrix size: ", size(α_matrix))
 
-  recurrence_matrix = construct_recurrence_matrix(n, matrix_size)
+  recurrence_matrix = construct_recurrence_matrix(n, matrix_size, polynomial_degree)
   println("Recurrence matrix size: ", size(recurrence_matrix))
 
   return dot(recurrence_matrix, α_matrix)
@@ -110,8 +110,9 @@ function generate_random_ode_dataset()
     println("Generated α matrix:")
     display(α_matrix)
 
-    recurrence = generate_recurrence_relation(α_matrix)
-    println("Recurrence relation: ", recurrence, " = 0")
+    recurrence = generate_recurrence_relation(α_matrix, poly_degree)
+    remove_this_symbol_already = clean_recurrence_display(recurrence)
+    println("Recurrence relation: ", remove_this_symbol_already, " = 0")
   end
 end
 
